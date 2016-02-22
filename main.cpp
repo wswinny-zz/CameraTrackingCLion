e4200c51023847f42416e645c14d0376ddfff0c4
Aaron Hitchcock <hitchcock9307@gmail.com>
2016-02-22 16:15:25 -0600

    - Added fisheye distortion removal, needs proper XML file of camera coefficients - Fixed error where the viewing frustum was being shifted in the +y axis by half of the frustum width - Error where frustum distance was being calculated in a rectangular manner instead of a curved manner, utilized pythagoras theorem to get hypotenuse distance from the camera to the frustum point

--- main.cpp
+++ main.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
-#include <string.h>
+#include <string>
 #include <stdio.h>
 #include "glm/glm.hpp"
 #include "glm/gtx/transform.hpp"
+
+/*
+ <Camera_Matrix type_id="opencv-matrix">
+  <rows>3</rows>
+  <cols>3</cols>
+  <dt>d</dt>
+  <data>
+    1.9982570939581169e+003 0. 3.1950000000000000e+002 0.
+    1.9982570939581169e+003 2.3950000000000000e+002 0. 0. 1.</data></Camera_Matrix>
+<Distortion_Coefficients type_id="opencv-matrix">
+  <rows>5</rows>
+  <cols>1</cols>
+  <dt>d</dt>
+  <data>
+    6.9509977809186782e-003 -8.9345795644874160e-001 0. 0.
+    1.7167890387968214e+001</data></Distortion_Coefficients>
+ */
+
 
 using namespace cv;
 
@@ -11,32 +29,44 @@ const float WIDTH = 640;
 const float HEIGHT = 480;
 
 const float FOV = 60.0f;
+const float f = 2.8/1000;
 
-const float FRUSTDIST = 1.0f;
+const float FRUSTDIST = 0.5f;
 
-const float ASPECTRATIO = WIDTH/(float)HEIGHT;
+const float ASPECTRATIO = WIDTH/HEIGHT;
 const float FRUSTWIDTH = FRUSTDIST * glm::tan(glm::radians(FOV/2)) * 2;
 const float FRUSTHEIGHT = FRUSTWIDTH * (1/ASPECTRATIO);
 
-float cPitch = 30;
-float cYaw = (90 - 66.33);
+float cPitch = 20.5;
+float cYaw = 69;
 
-//const std::string videoAddr = "http://admin:103iCorE@192.168.103.59/video.cgi?.mjpg"; //icore camera far wall
+const std::string videoAddr = "http://admin:103iCorE@192.168.103.59/video.cgi?.mjpg"; //icore camera far wall
 //const std::string videoAddr = "http://admin:103iCorE@192.168.103.180/video.cgi?.mjpg"; //icore camera door
-const std::string videoAddr = "http://plazacam.studentaffairs.duke.edu/mjpg/video.mjpg"; //duke university plaza
+//const std::string videoAddr = "http://plazacam.studentaffairs.duke.edu/mjpg/video.mjpg"; //duke university plaza
 
+const std::string icoreFloorPlanImage = "../../iCORE_floor_plan.jpg";
+const int cameraPixelX = 130;
+const int cameraPixelY = 960;
+
+const float pixelToMeterRatio = 0.01209; //meters in one pixel
+const float pixelToFeetRatio = 0.476; //feet in one pixel
 
 //Calculate coordinate of pixel in space assuming that the camera is facing down the
 //X axis with Z upwards and Y 90deg to the left, pixels start from lowerLeft
 glm::vec3 frustrumCoord(int x, int y)
 {
+    const Point center = {WIDTH/2,HEIGHT/2};
+
     float percentY = x/WIDTH; //Here the coordinates are translated from image coordinates to the local axis for a +x camera
     float percentZ = (HEIGHT-y)/HEIGHT; //x->y, y->z and possibly top left to lower left conversion
 
     float yPoint = percentY * FRUSTWIDTH;
     float zPoint = percentZ * FRUSTHEIGHT;
 
-    return glm::vec3(FRUSTDIST, yPoint, zPoint);
+
+    float realFrustDist = sqrt(yPoint*yPoint + zPoint*zPoint + FRUSTDIST*FRUSTDIST);
+
+    return glm::vec3(realFrustDist, yPoint-FRUSTWIDTH/2, zPoint-FRUSTHEIGHT/2);
 }
 
 glm::vec3 getPointFromC(int x, int y){
@@ -50,17 +80,17 @@ glm::vec3 getPointFromC(int x, int y){
     cameraRot = glm::rotate(cameraRot, glm::radians(cPitch), glm::vec3(0,1,0)); //Pitch rotate
 
 
-    glm::mat4 frustTranslate = glm::translate(glm::mat4(1.0f), glm::vec3(0, FRUSTWIDTH/2, -FRUSTHEIGHT/2));
+    //glm::mat4 frustTranslate = glm::translate(glm::mat4(1.0f), glm::vec3(0, FRUSTWIDTH/2, -FRUSTHEIGHT/2));
     glm::vec3 frustCoord = frustrumCoord(x, y);
-    frustCoord = glm::vec3(frustTranslate * glm::vec4(frustCoord, 1));
     frustCoord = glm::vec3(cameraRot * glm::vec4(frustCoord, 1));
+    //frustCoord = glm::vec3(frustTranslate * glm::vec4(frustCoord, 1));
 
     return frustCoord;
 }
 
 glm::vec3 getIntersect(int x, int y)
 {
-    glm::vec3 camera = glm::vec3(0, 0, 3.3f);
+    glm::vec3 camera = glm::vec3(0, 0, 3.2f);
     glm::vec3 point = getPointFromC(x, y) + camera;
     glm::vec3 p0 = glm::vec3(0,0,0);
     glm::vec3 normal = glm::vec3(0,0,1);
@@ -83,17 +113,66 @@ int main (int argc, const char * argv[])
         return -1;
     }
 
+    Mat icoreFloorPlan;
     Mat img;
     HOGDescriptor hog;
     hog.setSVMDetector(HOGDescriptor::getDefaultPeopleDetector());
 
     namedWindow("video capture", CV_WINDOW_AUTOSIZE);
+    namedWindow("floor plan", CV_WINDOW_AUTOSIZE);
+
+    icoreFloorPlan = imread(icoreFloorPlanImage, CV_LOAD_IMAGE_COLOR);
+
+//    CvMat* intrinsics = cvCreateMat(3, 3, CV_64FC1);
+//
+//
+//    IplImage tmp=img;
+//
+//    CvArr* mapx = cvCreateImage(cvGetSize(&tmp),IPL_DEPTH_32F, 1);
+//    CvArr* mapy = cvCreateImage(cvGetSize(&tmp), IPL_DEPTH_32F, 1);
+//    cvInitUndistortMap(intrinsics, dist_coeffs, mapx, mapy)
+//    cvRemap(src, dst, mapx, mapy, cv.CV_INTER_LINEAR + cv.CV_WARP_FILL_OUTLIERS,  cv.ScalarAll(0))
+
+
+
+    if(!icoreFloorPlan.data)
+    {
+        printf("No floor plan data");
+        return -1;
+    }
 
     while (true)
     {
         cap >> img;
         if (!img.data)
             continue;
+
+//
+//        out1.create(3,3, CV_64F);
+//        out2.create(3,3, CV_64F);
+
+
+        FileStorage fs_in;
+        fs_in.open( "out_camera_data.xml", FileStorage::READ );
+
+
+        cv::Mat out1, out2, undist, cameraMatrix, distMatrix;
+
+        undist.create(img.size(), img.type());
+        fs_in["Camera_Matrix"] >> cameraMatrix;
+        fs_in["Distortion_Coefficients"] >> distMatrix;
+
+
+        cv::initUndistortRectifyMap(cameraMatrix, distMatrix, cv::Mat(), cameraMatrix, img.size(), CV_16SC2, out1, out2);
+        cv::remap(img, undist, out1, out2, INTER_LINEAR);
+
+        img = undist;
+        //icoreFloorPlan = imread(icoreFloorPlanImage, CV_LOAD_IMAGE_COLOR);
+        if(!icoreFloorPlan.data)
+        {
+            printf("No floor plan data");
+            return -1;
+        }
 
         std::vector<Rect> found, found_filtered;
         hog.detectMultiScale(img, found, 0, Size(4,4), Size(6,6), 1.30, 4);
@@ -113,7 +192,7 @@ int main (int argc, const char * argv[])
                 found_filtered.push_back(r);
         }
 
-        printf("\x1B[2J\x1B[H"); //clears the console in unix systems
+        //printf("\x1B[2J\x1B[H"); //clears the console in unix systems
 
         for (i = 0; i < found_filtered.size(); i++)
         {
@@ -126,13 +205,21 @@ int main (int argc, const char * argv[])
             float feetX = r.x + (r.width / 2.0);
             float feetY = r.y + r.height;
 
-            //printf("x: %08.2f y: %08.2f \n", feetX, feetY);
-            getIntersect((int)feetX, (int)feetY);
+            glm::vec3 person = getIntersect((int)feetX, (int)feetY);
+            int feetPixX = person.y / pixelToMeterRatio;
+            int feetPixY = person.x / pixelToMeterRatio;
 
+
+
+            circle(icoreFloorPlan, CvPoint(feetPixX + cameraPixelX, cameraPixelY - feetPixY), 2, cv::Scalar(255, 0, 0), 4);
             rectangle(img, r.tl(), r.br(), cv::Scalar(0,255,0), 2);
         }
 
+        circle(icoreFloorPlan, CvPoint(cameraPixelX, cameraPixelY), 2, cv::Scalar(0, 0, 255), 4);
+
         imshow("video capture", img);
+        imshow("undist", undist);
+        imshow("floor plan", icoreFloorPlan);
 
         if (waitKey(20) >= 0)
             break;
